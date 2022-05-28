c85b6f79bba757599311584bd45f7369b3ffd7b7
Kartikey <ranjankartikey1999@gmail.com>
2022-05-28 16:38:20 +0530

    fixed gh-pages url

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import {getAuth, onAuthStateChanged} from 'firebase/auth';
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import UserInfoForm from './pages/UserInfoForm';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-
+import React from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import UserInfoForm from "./pages/UserInfoForm";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-
   const auth = getAuth();
-  const [user,setUser] = React.useState(null);
+  const [user, setUser] = React.useState(null);
 
-  React.useEffect(()=>{
-    onAuthStateChanged(auth,(user)=>{
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
       setUser(user);
-    })
-  },[auth])
-
+    });
+  }, [auth]);
 
   return (
     <Router>
-      <Routes>     
-        <Route path="/" element={<Home user={user} />}/>
-        <Route path="/signin" element={ <Signin/> }/>
-        <Route path="/signup" element={user ? <Navigate to='/signin' /> :<Signup/>}/>
-        <Route path="/form" element={<UserInfoForm/>}/>
-        <Route path='/profile' element={<Profile user={user} />}/>
+      <Routes>
+        <Route path="/" element={<Home user={user} />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route
+          path="/signup"
+          element={user ? <Navigate to="/signin" /> : <Signup />}
+        />
+        <Route path="/form" element={<UserInfoForm />} />
+        <Route path="/profile" element={<Profile user={user} />} />
       </Routes>
     </Router>
   );
